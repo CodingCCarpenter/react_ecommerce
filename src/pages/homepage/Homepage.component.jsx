134a4a320b6b10b9a943cd33f2ed67b7a4c80997
CodingCCarpenter <Christineassists@aol.com>
2020-08-25 15:10:35 -0500

    undo prop drilling

--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -8,7 +8,7 @@ import Directory from '../../components/directory/directory.component'
 
 const HomePage = ({ history }) => (
     <div className='homepage'>
-        < Directory history={history}/>
+        < Directory />
         
     </div>
 )
