26d7bcd81c2c4afe261183978b60db99caad7d79
CodingCCarpenter <Christineassists@aol.com>
2020-08-24 22:35:08 -0500

    troubleshoot import and distribute homepage.styles.scss code to individual components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+// IMPORT STYLE
+import './directory.styles.scss';
+
--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 import './homepage.styles.scss';
 
 // IMPORT COMPONENTS
-import MenuItem from '../../components/menu-item'
+import MenuItem from '../../components/menu-item/menu-item.component';
+
 const HomePage = () => (
     <div className='homepage'>
         <div className='directory-menu'>
