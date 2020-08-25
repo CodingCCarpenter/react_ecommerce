565285b0470792cf50f074ea217e1cf1770853e1
CodingCCarpenter <Christineassists@aol.com>
2020-08-25 15:26:42 -0500

    add annotations

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,15 @@
+// REACT IMPORTS
 import React from 'react';
-
-import './menu-item.styles.scss'
 import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size, history }) => (
-    <div className={`${size} menu-item`} onClick={() => history.push()}>
+// STYLE IMPORT
+import './menu-item.styles.scss'
+
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div 
+        className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div 
             className='background-image'
             style ={{ backgroundImage: `url(${imageUrl})` }} />
