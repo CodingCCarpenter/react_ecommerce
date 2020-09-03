770583f7c23122023c6b07a6ee62c6a3ca7d6097
CodingCCarpenter <Christineassists@aol.com>
2020-09-03 15:25:48 -0500

    add logo to assets and create basic scaffolding for Header component

---
+++ src/components/header/Header.component.jsx
@@ -0,0 +1,16 @@
+// REACT IMPORTS
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// STYLE IMPORT
+import './header.styles.scss';
+
+const Header = () => (
+    <div className='header'>
+        <Link to='../../assets/crown.svg'>
+
+        </Link>
+    </div>
+);
+
+export default Header;
