750017706864347fb882cd5be1b77a81a34ab198
CodingCCarpenter <Christineassists@aol.com>
2020-09-02 22:16:42 -0500

    set items on shop page to map through items in list and limit the results to 4

--- src/pages/shopPage/shopPage.component.jsx
+++ src/pages/shopPage/shopPage.component.jsx
@@ -12,16 +12,21 @@ class ShopPage extends React.Component {
 
         this.state = {
             collections: SHOP_DATA
-        } 
+        };
     }
 
     render() {
-        
+        const {collections} = this.state;
+
         return(
-            <div>
-                <CollectionPreview />
+            <div className='shop-page'>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
             </div>
-        )
+        );
     }
 }
 
