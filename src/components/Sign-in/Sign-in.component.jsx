d0bdcac18cd446133bf9a913cb551a0534a030f1
CodingCCarpenter <Christineassists@aol.com>
2020-09-04 16:04:28 -0500

    import Google authentication into sign in component; annotate imports in sign-in component

--- src/components/Sign-in/Sign-in.component.jsx
+++ src/components/Sign-in/Sign-in.component.jsx
@@ -5,6 +5,8 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/Form-input.component';
 import CustomButton from '../custom-button/Custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends React.Component{
     constructor(props) {
         super(props);
