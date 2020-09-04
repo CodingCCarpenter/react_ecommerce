3619f5fec2289430e26382a648751c0b4b167581
CodingCCarpenter <Christineassists@aol.com>
2020-09-04 16:14:36 -0500

    convert app.js to a class component for ease of state management

--- src/App.js
+++ src/App.js
@@ -11,18 +11,24 @@ import ShopPage from './pages/shopPage/ShopPage.component';
 import Header from './components/header/Header.component';
 import SignInAndSignUpPage from './components/Sign-in-and-sign-up/Sign-in-and-sign-up.component';
 
+// FIREBASE AUTHENTICATION IMPORT
+import { auth } from './firebase/firebase.utils';
+import { render } from 'node-sass';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={ HomePage }/>
-        <Route path='/shop' component={ ShopPage }/>
-        <Route path='/signin' component={ SignInAndSignUpPage }/>
-      </Switch>
-    </div>
-  );
+
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={ HomePage }/>
+          <Route path='/shop' component={ ShopPage }/>
+          <Route path='/signin' component={ SignInAndSignUpPage }/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
