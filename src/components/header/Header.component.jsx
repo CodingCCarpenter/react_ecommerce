ea98b7722398e1ccfa629a3847809c770c997605
CodingCCarpenter <Christineassists@aol.com>
2020-09-08 21:32:25 -0500

    add sign out functionality as well as SIGN IN and SIGN OUT options in the menu which render based on whether user is currently logged in

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -8,7 +8,10 @@ import './header.styles.scss';
 // ASSET IMPORT
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = () => (
+// FIREBASE AUTH IMPORT
+import { auth } from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
     <div className='header'>
 
         <Link className='logo-container' to='/'>
@@ -16,15 +19,23 @@ const Header = () => (
         </Link>
 
         <div className='options'>
-
             <Link className='option' to='/shop'>
                 SHOP
             </Link>
-
             <Link className='option' to='/contact'>
                 CONTACT
             </Link>
-            
+            {
+                currentUser ? (
+                    <div className='option' onClick={() => auth.signOut()}>
+                        SIGN OUT
+                    </div>
+                ) : (
+                    <Link className='option' to='/signin'>
+                        SIGN IN
+                    </Link>
+                )
+            }
         </div>
     </div>
 );
